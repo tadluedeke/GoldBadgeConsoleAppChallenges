d9a1cb4e454204b7f24614a24bcfca298a7d297b
Tad Luedeke <tad.luedeke@gmail.com>
2021-05-16 14:40:53 -0400

    screwed up naming conventions, attempting to fix.

--- ChallengeTwo.Console/ProgramUI.cs
+++ ChallengeTwo.Console/ProgramUI.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using ChallengeTwo.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ChallengeTwo.Console
+namespace ChallengeTwo_Console
 {
     class ProgramUI
     {
+        private ClaimRepository _repo = new ClaimRepository();
+        public void Run()
+        {
+            Menu();
+        }
+
+        private void Menu()
+        {
+            bool keepRunning = true;
+            while (keepRunning)
+            {
+                Console.WriteLine("Select a menu option: \n" +
+                    "1. See All Claims\n" +
+                    "2. Take Care of Next Claim\n" +
+                    "3. Enter a New Claim");
+
+                string input = Console.ReadLine();
+            }
+        }
     }
 }
