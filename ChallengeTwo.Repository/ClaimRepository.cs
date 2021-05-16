313879745900bbbc269c0461956ecdfe9e07f659
Tad Luedeke <tad.luedeke@gmail.com>
2021-05-16 14:24:03 -0400

    Could not figure out Peek. Moving on.

--- ChallengeTwo.Repository/ClaimRepository.cs
+++ ChallengeTwo.Repository/ClaimRepository.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace ChallengeTwo.Repository
 {
-    class ClaimRepository
+    public class ClaimRepository
     {
+        private readonly Queue<Claim> _claimQueue = new Queue<Claim>();
+
+        public bool AddClaimToQueue(Claim newClaim)
+        {
+            int startingCount = _claimQueue.Count;
+            _claimQueue.Enqueue(newClaim);
+            bool wasAdded = (_claimQueue.Count > startingCount) ? true : false;
+            return wasAdded;
+        }
+
+        public Queue<Claim> SeeAllClaims()
+        {
+            return _claimQueue;
+        }
     }
 }
